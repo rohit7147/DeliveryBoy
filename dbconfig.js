1c0463b12c22735339e837415171f79976d4fce5
rohit7147 <32077823+rohit7147@users.noreply.github.com>
2020-04-24 00:47:03 +0530

    Update dbconfig.js

--- dbconfig.js
+++ dbconfig.js
@@ -2,13 +2,13 @@ import * as firebase from 'firebase'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBpuGigdN61FrhOduAPs5bB_trR7_1mdQ0",
-  authDomain: "deliveryboy-8cd22.firebaseapp.com",
-  databaseURL: "https://deliveryboy-8cd22.firebaseio.com",
-  projectId: "deliveryboy1-8cd22",
-  storageBucket: "deliveryboy-8cd22.appspot.com",
-  messagingSenderId: "778407923162",
-  appId: "1:778407923162:web:6a9b7d3017d602a4eac4e3"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 if (!firebase.apps.length) {
